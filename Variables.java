911053412384d6e3b27f30027d5e1391818f99fd
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-05-01 13:06:28 +0500

    learn java

---
+++ Variables.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Variables {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter something >>>");
+
+        int num = myObj.nextInt();
+        System.out.println(num+345);
+
+
+    }
+}
